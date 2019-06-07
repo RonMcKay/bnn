e2b062620739365b6a74669342f99a3bda08f2a8
Philipp Oberdiek <philipp.oberdiek@cs.tu-dortmund.de>
2019-06-07 14:45:53 +0200

    Add priors and update other stuff
    - Added an own prior wrapper around the pytorch distributions so that
      they can be ported to the gpu more easily.
    - Means of variational distributions are now initialized uniformly and
      with an slightly larger standard deviation.
    - Updated the parameterization of the variational standard deviation
      from log(exp(rho)-1) to exp(rho) as this seems to be more stable.

--- bnn/fc_layers.py
+++ bnn/fc_layers.py
@@ -1,44 +1,64 @@
+import math
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+# import torch.distributions as dist
 from torch.distributions import Distribution
 from .general import BayesianLayer
 from .vardist import VariationalDistribution
+from .vardist import DiagonalNormal
+from .priordist import DiagonalNormal as PriorNormal
 import logging
 
 class BLinear(BayesianLayer):
     """Applies a bayesian linear transformation to the incoming data.
     """
-    def __init__(self, in_features, out_features, prior, var_dist, bias=False,
-                 bias_prior=None, bias_var_dist=None):
+    def __init__(self, in_features, out_features, prior=None, var_dist=None,
+                 bias=True, bias_prior=None, bias_var_dist=None):
         super().__init__()
         self.log = logging.getLogger(__name__)
         self.in_features = in_features
         self.out_features = out_features
                 
-        if not isinstance(prior, Distribution):
+        if prior is None:
+            self.prior = PriorNormal(loc=torch.zeros((out_features, in_features)),
+                                     scale=torch.ones((out_features, in_features)))
+        elif not isinstance(prior, Distribution):
             self.log.error('prior has to be a pytorch distribution')
             raise ValueError('prior has to be a pytorch distribution')
-        self.prior = prior
+        else:
+            self.prior = prior
         
-        if not isinstance(var_dist, VariationalDistribution):
+        if var_dist is None:
+            std = 1.0 / math.sqrt(self.out_features)
+            self.var_dist = DiagonalNormal(mean=torch.Tensor(out_features, in_features).uniform_(-std, std),
+                                           rho=torch.full((out_features, in_features), 0.05))
+        elif not isinstance(var_dist, VariationalDistribution):
             self.log.error('var_dist has to be a variational distribution')
             raise ValueError('var_dist has to be a variational distribution')
-        self.var_dist = var_dist
+        else:
+            self.var_dist = var_dist
+            
         self.bias = bias
+        if self.bias:
+            if bias_prior is None:
+                self.bias_prior = PriorNormal(loc=torch.zeros((out_features,)),
+                                              scale=torch.full((out_features,), 1.05))
+            elif not isinstance(bias_prior, Distribution):
+                self.log.error('bias_prior has to be a pytorch distribution')
+                raise ValueError('bias_prior has to be a pytorch distribution')
+            else:
+                self.bias_prior = bias_prior
         
-        if not isinstance(bias_prior, Distribution):
-            self.log.error('bias_prior has to be a pytorch distribution')
-            raise ValueError('bias_prior has to be a pytorch distribution')
-        self.bias_prior = bias_prior
-        
-        if not isinstance(bias_var_dist, VariationalDistribution):
-            self.log.error('bias_var_dist has to be a variational distribution')
-            raise ValueError('bias_var_dist has to be a variational distribution')
-        self.bias_var_dist = bias_var_dist
-        
-        if self.bias and (self.bias_prior is None or self.bias_var_dist is None):
-            self.log.error('If bias is true, bias_prior and bias_var_dist have to be specified.')
+            if bias_var_dist is None:
+                std = 1.0 / math.sqrt(self.out_features)
+                self.bias_var_dist = DiagonalNormal(mean=torch.Tensor(out_features).uniform_(-std, std),
+                                                    rho=torch.full((out_features,), 0.05))
+            elif not isinstance(bias_var_dist, VariationalDistribution):
+                self.log.error('bias_var_dist has to be a variational distribution')
+                raise ValueError('bias_var_dist has to be a variational distribution')
+            else:
+                self.bias_var_dist = bias_var_dist
         
     def reset_parameters(self):
         raise NotImplementedError('Has to be implemented!')
