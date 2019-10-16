01f071c5fd37e2b1626f2d783a2186f188de08c6
Philipp Oberdiek <philipp.oberdiek@cs.tu-dortmund.de>
2019-10-16 13:39:19 +0200

    Add Sequential container

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .utils.conv_layers import *
 from .utils.linear_layers import *
+from .utils.general import Sequential
--- utils/general.py
+++ utils/general.py
@@ -2,11 +2,68 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import OrderedDict
 import logging
+import operator
 
 class BayesianLayer(nn.Module):
     def __init__(self):
         super().__init__()
+        
+class Sequential(nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        if len(args) == 1 and isinstance(args[0], OrderedDict):
+            for key, module in args[0].items():
+                self.add_module(key, module)
+        else:
+            for idx, module in enumerate(args):
+                self.add_module(str(idx), module)
+                
+    def _get_item_by_idx(self, iterator, idx):
+        """Get the idx-th item of the iterator"""
+        size = len(self)
+        idx = operator.index(idx)
+        if not -size <= idx < size:
+            raise IndexError('index {} is out of range'.format(idx))
+        idx %= size
+        return next(islice(iterator, idx, None))
+    
+    def __getitem__(self, idx):
+        if isinstance(idx, slice):
+            return self.__class__(OrderedDict(list(self._modules.items())[idx]))
+        else:
+            return self._get_item_by_idx(self._modules.values(), idx)
+        
+    def __setitem__(self, idx, module):
+        key = self._get_item_by_idx(self._modules.keys(), idx)
+        return setattr(self, key, module)
+    
+    def _delitem__(self, idx):
+        if isinstance(idx, slice):
+            for key in list(self._modules.keys())[idx]:
+                delattr(self, key)
+        else:
+            key = self._get_item_by_idx(self._modules.keys(), idx)
+            delattr(self, key)
+            
+    def __len__(self):
+        return len(self._modules)
+    
+    def __dir__(self):
+        keys = super().__dir__()
+        keys = [key for key in keys if not key.isdigit()]
+        return keys
+                
+    def forward(self, input):
+        kl = torch.tensor(0.0, device=input.device, requires_grad=True)
+        for module in self._modules.values():
+            if isinstance(module, BayesianLayer):
+                input, _kl = module(input)
+                kl = kl + _kl
+            else:
+                input = module(input)
+        return input, kl
         
 class KLLoss(nn.Module):
     def __init__(self, ignore_index=-100, weight=None, reduction='sum'):
